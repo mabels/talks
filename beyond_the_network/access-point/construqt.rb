870d8a20efbbad488939e92f608c8aaa2abbe2cd
Meno Abels <meno.abels@adviser.com>
2015-10-26 17:05:41 +0100

    added @cramforce feedback, less code

--- beyond_the_network/access-point/construqt.rb
+++ beyond_the_network/access-point/construqt.rb
@@ -1,27 +1,6 @@
 require_relative "../core_net"
 
-require 'enumerator'
-
-def distil_wifi_ap(cn)
-  cn.region.hosts.add('wifi-ap', "flavour" => "mikrotik") do |ap|
-    ap.configip = ap.id ||= Construqt::HostId.create do |my|
-      my.interfaces << cn.region.interfaces.add_bridge(ap, "bridge-local",
-      "mtu" => 1500, "address" => cn.network.addresses.add_ip("192.168.88.0/24"),
-      "interfaces" => [
-        cn.region.interfaces.add_device(ap, "ether2", "mtu" => 1500, "default_name" => "ether2"),
-        master_if = cn.region.interfaces.add_wlan(ap, "wlan1", "mtu" => 1500,
-          "default_name" => "wlan1", "band" => "2ghz-b/g/n", "hide_ssid" => true,
-          "channel_width" => "20/40mhz-Ce", "country" => "germany",
-          "mode" => "ap-bridge", "rx_chain" => "0,1", "tx_chain" => "0,1",
-          "ssid" => Digest::SHA256.hexdigest("wifi-geheim")[0..12],
-          "psk" => Digest::SHA256.hexdigest("wifi-geheim")[12..28])]+
-        (["wifi-con", "wifi-struqt"].map.with_index(300).map do |ssid, vlan|
-          cn.region.interfaces.add_wlan(ap, "wl#{ssid}", "mtu" => 1500, "vlan_id" => vlan,
-          "master_if" => master_if, "ssid" => ssid.upcase, "psk" => "#{ssid}-#{vlan}")
-        end))
-  	end
-  end
-end
+require_relative "./distil_wifi_ap.rb"
 
 core_net = CoreNet.setup
 distil_wifi_ap(core_net)
--- beyond_the_network/blended-scotch/construqt.rb
+++ beyond_the_network/blended-scotch/construqt.rb
@@ -1,25 +1,36 @@
 require_relative "../core_net"
 
+require_relative "../access-point/distil_wifi_ap.rb"
 
 def distil_blended_scotch(cn)
   [300,301].each do |vlan|
-    cn.region.hosts.add("blended-scotch-#{vlan}", "flavour" => "ubuntu") do |host|
+    cn.region.hosts.add("blended-scotch-#{vlan}", "flavour" => "ubuntu",
+                        "mother" => cn.vagrant_host, "vagrant_deploy" => []) do |host|
       cn.region.interfaces.add_device(host, "lo", "mtu" => "9000", "address" =>
         cn.region.network.addresses.add_ip(Construqt::Addresses::LOOOPBACK))
       host.configip = host.id ||= Construqt::HostId.create do |my|
         my.interfaces << cn.region.interfaces.add_device(host, "eth0", "mtu" => 1500,
+         "plug_in" => Construqt::Cables::Plugin.new.iface(cn.mac_wlan),
          "address" => cn.network.addresses.add_ip(Construqt::Addresses::DHCPV4))
       end
-      cn.region.interfaces.add_device(host, "v#{vlan}", "mtu" => 1500, 'address' =>
-        cn.region.network.addresses.add_ip("192.168.#{vlan%256}.1/24#INTERNAL-NET",
-        "dhcp_range" => ["192.168.#{vlan%256}.100", "192.168.#{vlan%256}.200"]))
+      cn.region.interfaces.add_vlan(host, "eth1.#{vlan}", "mtu" => 1500, "vlan_id" => vlan,
+          "interface" => cn.region.interfaces.add_device(host, "eth1", "mtu" => 1500,
+            "plug_in" => Construqt::Cables::Plugin.new.iface(cn.mac_usb_ether)),
+          'address' => cn.region.network.addresses.add_ip("192.168.#{vlan%256}.1/24#INTERNAL-NET",
+          "dhcp_range" => ["192.168.#{vlan%256}.100", "192.168.#{vlan%256}.200"]))
     end
   end
 end
 
 core_net = CoreNet.setup
 
+distil_wifi_ap(core_net)
 distil_blended_scotch(core_net)
+
+core_net.region.cables.add(
+  core_net.region.interfaces.find("wifi-ap", "ether2"),
+  core_net.region.interfaces.find("vagrant-host", "en3")
+)
 
 core_net.produce
 
