431679b872ea73690072b9b0106754ce8bed0d81
Meno Abels <meno.abels@adviser.com>
2017-07-07 14:08:37 +0200

    * WIP

--- is-this-simple/src/azure/webpack.config.js
+++ is-this-simple/src/azure/webpack.config.js
@@ -2,9 +2,9 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-  entry: './handler.js',
+  entry: './azure.js',
   output: {
-    path: './bin',
-    filename: 'app.bundle.js'
+    path: __dirname,
+    filename: 'handler.js'
   }
 };
